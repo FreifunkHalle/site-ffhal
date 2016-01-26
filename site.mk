11a6d3aa2a312e185bcbe5577257ef50f3a2f703
Steffen Taubenheim-Probst <s.taubenheim@tux-mania.de>
2016-01-26 17:07:27 +0100

    site.mk auf stable umgestellt

--- site.mk
+++ site.mk
@@ -26,13 +26,13 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.7~exp$(shell date '+%Y%m%d%H%M')
+DEFAULT_GLUON_RELEASE := 0.7~stable$(shell date '+%Y%m%d%H')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
-GLUON_BRANCH ?= experimental
+GLUON_BRANCH ?= stable 
 export GLUON_BRANCH
 
 GLUON_TARGET ?= ar71xx-generic
