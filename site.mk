90c1fc31cd037566588908f76ed3048bb8501d7f
Steffen Taubenheim-Probst <s.taubenheim@tux-mania.de>
2016-02-08 14:04:25 +0100

    Datum für Releasenummer erweiter auf Stunden und Minuten. Führt sonst dazu, das kein autoupdate gefahren wird bei den nächsten Releases

--- site.mk
+++ site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.7~stable$(shell date '+%Y%m%d%H')
+DEFAULT_GLUON_RELEASE := 0.7~stable$(shell date '+%Y%m%d%H%M')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
