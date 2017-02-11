d2001d32a640bb8695465f0fc0eeeb3e97c2deff
Steffen Taubenheim-Probst <s.taubenheim@tux-mania.de>
2017-02-11 19:22:06 +0100

    auf experimental umgestellt

--- site.mk
+++ site.mk
@@ -29,13 +29,13 @@ GLUON_SITE_PACKAGES := \
 	ffharz-autoupdater-path 
 
 
-DEFAULT_GLUON_RELEASE := 0.8.35~testing$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.8.35~exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
-GLUON_BRANCH ?= testing
+GLUON_BRANCH ?= experimental 
 export GLUON_BRANCH
 
 #GLUON_TARGET ?= ar71xx-generic
